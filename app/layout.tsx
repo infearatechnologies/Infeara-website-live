2a0591892dc8478825d97ca5385a46eda79b9c3b
infearatechnologies <info@infeara.com>
2025-12-21 00:03:51 +0530

    Update website content and fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,6 +92,19 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RCNB7MXXCQ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-RCNB7MXXCQ');
+          `}
+        </Script>
         {children}
         <Toaster />
       </body>
